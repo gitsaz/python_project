370836f2e520b0ad6a5c01c1474ccb08ff54b418
Sazzadur Rahman <sazzadur570@gmail.com>
2025-01-09 15:56:14 +0600

    Add files via upload
    new

---
+++ python_project/calculator.py
@@ -0,0 +1,14 @@
+num1 = float(input("enter the first number:"))
+mun2 = float(input("enter the second number:"))
+sing = ("enter a sing thah you want to calculate:")
+if sing == "+":
+  print("the ans is:",num1+num2)
+elif sing == "-":
+  print("the ans is:",num1-num2)
+elif sing == "*":
+  print("the ans is:",num1*num2)
+elif sing == "/":
+  print("the ans is:",num1/num2)
+else:
+  print("please enter +,-,*/")
+
